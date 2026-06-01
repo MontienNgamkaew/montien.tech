02036136ffa312d539e6cbb5a3a95114e38004f9
montien1992 <montien.ng@gmail.com>
2026-06-01 18:53:31 +0700

    fix: update production base_path in config/app.php to support /pnp-go subdirectory

--- pnp-go/config/app.php
+++ pnp-go/config/app.php
@@ -5,7 +5,7 @@ $isLocal = in_array($_SERVER['HTTP_HOST'] ?? '', ['localhost', '127.0.0.1']);
 
 return [
     'name'      => 'ระบบขออนุญาตใช้รถยนต์/สั่งซื้อน้ำมันเชื้อเพลิง',
-    'base_path' => $isLocal ? '/pnp-portal/pnp-go' : '',   // subdomain ใช้ ''
+    'base_path' => $isLocal ? '/pnp-portal/pnp-go' : '/pnp-go',   // โฟลเดอร์ย่อยบน production คือ /pnp-go
     'timezone'  => 'Asia/Bangkok',
     'debug'     => $isLocal,                         // ปิด debug บน production
 ];
